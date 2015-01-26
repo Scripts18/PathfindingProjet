6f9844d5ac1a9c62764c8a4fd175043c50e42eb0
Marc-André Larochelle <scripts18@hotmail.com>
2015-01-26 08:11:35 -0500

    Commit v1.0

---
+++ PathfindingProjet/Assets/Scripts/Map/Node.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	[SerializeField]private Vector2 position;
+
+	public Node(int x, int y)
+	{
+		this.position = this.transform.position;
+	}
+
+	void Start () 
+	{
+		this.gameObject.name = "(" + this.gameObject.transform.position.x.ToString() + ", " + this.gameObject.transform.position.y.ToString() + ")";
+	}
+}
---
+++ PathfindingProjet/Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour 
+{
+
+	[SerializeField]private Vector2 position;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
