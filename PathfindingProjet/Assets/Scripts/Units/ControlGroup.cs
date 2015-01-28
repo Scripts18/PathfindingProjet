a8644182bf5e710936c3336956f1a95912f253d0
Tyggerr <Tyggerr>
2015-01-28 11:29:01 -0500

    Formation

--- PathfindingProjet/Assets/Scripts/Units/ControlGroup.cs
+++ PathfindingProjet/Assets/Scripts/Units/ControlGroup.cs
@@ -8,18 +8,5 @@ public abstract class ControlGroup : MonoBehaviour
     public abstract void moveToPosition(int _x, int _y);
     public abstract void moveToPosition(GameObject _gameobject);
 
-    protected Vector3 center;
-
-    // Update is called once per frame
-    void Update()
-    {
-        this.calculateCenter();
-    }
-
-    protected abstract void calculateCenter();
-
-    public Vector3 getCenter()
-    {
-        return this.center;
-    }
+    public Vector3 offsetFromCenter;
 }
