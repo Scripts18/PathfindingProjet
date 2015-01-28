e423a0fd81ca23b5b0426d839bf3fee1ab4dfdc6
Marc-André Larochelle <scripts18@hotmail.com>
2015-01-28 12:28:25 -0500

    General Improvement

--- PathfindingProjet/Assets/Scripts/Units/Unit.cs
+++ PathfindingProjet/Assets/Scripts/Units/Unit.cs
@@ -7,6 +7,8 @@ public class Unit : ControlGroup
 	[SerializeField]private Vector3 position;
     [SerializeField]private Vector3 target;
 
+    float step = 5 * Time.deltaTime;
+
     private Stack<Node> movementOrders = new Stack<Node>();
 
     //Temporary until UnitPathFinding
@@ -15,7 +17,8 @@ public class Unit : ControlGroup
     //Testing Purpose 
     [SerializeField] private Map currentMap;
 
-    public bool isMoving;
+    public bool isMoving = false;
+    private bool doNextMovement = true;
 
     public override void ComputePathfinding()
     {
@@ -66,16 +69,72 @@ public class Unit : ControlGroup
 	// Update is called once per frame
 	void Update () 
 	{
-        // The step size is equal to speed times frame time.
-        var step = 5 * Time.deltaTime;
-
         // Move our position a step closer to the target.
-        transform.position = Vector3.MoveTowards(transform.position, this.target, step);
-       
-       if (this.movementOrders.Peek() != null && this.transform.position == this.target)
+        this.transform.position = Vector3.MoveTowards(transform.position, this.target, this.step);
+
+       if (this.transform.position == this.target && this.isMoving)
        {
-           this.target = this.movementOrders.Pop().transform.position;
+           this.movementDone();
        }
+       else if (this.doNextMovement)
+       {
+           this.startMoving();
+       }
+    }
+
+    private void startMoving()
+    {
+        this.isMoving = true;
+    }
+
+    private void movementDone()
+    {
+        this.isMoving = false;
+
+        if (this.movementOrders.Peek() != null)
+        {
+            this.target = this.movementOrders.Pop().transform.position;
+            this.doNextMovement = true;
+        }
+    }
+
+    private void cancelMovements()
+    {
+        while (this.movementOrders.Peek() != null)
+        {
+            this.movementOrders.Pop();
+        }
+    }
+
+    private void changePath(Vector3 newOrder, bool queueMovement)
+    {
+        Stack<Node> newPath;
+
+        if (this.isMoving)
+        {
+            newPath = this.pathFinding.PathFinding(this.currentMap.MapTiles[(int)this.target.x][(int)this.target.y].GetComponent<Node>(), this.currentMap.MapTiles[(int)newOrder.x][(int)newOrder.y].GetComponent<Node>());
+        }
+        else
+        {
+            newPath = this.pathFinding.PathFinding(this.currentMap.MapTiles[(int)this.transform.position.x][(int)this.transform.position.y].GetComponent<Node>(), this.currentMap.MapTiles[(int)newOrder.x][(int)newOrder.y].GetComponent<Node>());
+        }
+
+        Stack<Node> concatStack = new Stack<Node>();
+
+        if (queueMovement)
+        {
+            this.cancelMovements();
+        }
+
+        for(int i = 0; i < newPath.Count - 1; i++)
+        {
+            concatStack.Push(newPath.Pop());
+        }
+
+        for (int i = 0; i < concatStack.Count - 1; i++)
+        {
+            this.movementOrders.Push(concatStack.Pop());
+        }  
     }
 
     public void ForceSetPosition(Vector3 _position)
